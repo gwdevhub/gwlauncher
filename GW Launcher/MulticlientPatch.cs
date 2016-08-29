4d507046b9b0e07c6f0496f131e8d90697ccd0af
Greg Landowski <greglando@gmail.com>
2016-08-28 19:35:36 -0700

    - actual launch code is now written in a c dll, completely written by me (thanks to tecka for picking out stupid things i did)  - import the LaunchClient function from the dll to create your own launcher, if you desire

--- GW Launcher/MulticlientPatch.cs
+++ GW Launcher/MulticlientPatch.cs
@@ -9,137 +9,24 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.IO;
 using GWCA.Memory;
-using GWMultiLaunch;
 using GW_Launcher;
 
 namespace GWMC_CS
 {
-    public struct STARTUPINFO
+
+    class MulticlientPatch
     {
-        public uint cb;
-        public string lpReserved;
-        public string lpDesktop;
-        public string lpTitle;
-        public uint dwX;
-        public uint dwY;
-        public uint dwXSize;
-        public uint dwYSize;
-        public uint dwXCountChars;
-        public uint dwYCountChars;
-        public uint dwFillAttribute;
-        public uint dwFlags;
-        public short wShowWindow;
-        public short cbReserved2;
-        public IntPtr lpReserved2;
-        public IntPtr hStdInput;
-        public IntPtr hStdOutput;
-        public IntPtr hStdError;
-    }
+        enum GWML_FLAGS {
+            NO_DATFIX        = 1,
+            KEEP_SUSPENDED   = 2,
+            NO_LOGIN		 = 4
+        };
 
-    public struct PROCESS_INFORMATION
-    {
-        public IntPtr hProcess;
-        public IntPtr hThread;
-        public uint dwProcessId;
-        public uint dwThreadId;
-    }
+        [DllImport("GWML.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        static extern uint LaunchClient(string client, string email, string pass, string charname, string args, GWML_FLAGS flags, out IntPtr thread);
 
-    [Flags]
-    public enum ProcessCreationFlags : uint
-    {
-        ZERO_FLAG = 0x00000000,
-        CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
-        CREATE_DEFAULT_ERROR_MODE = 0x04000000,
-        CREATE_NEW_CONSOLE = 0x00000010,
-        CREATE_NEW_PROCESS_GROUP = 0x00000200,
-        CREATE_NO_WINDOW = 0x08000000,
-        CREATE_PROTECTED_PROCESS = 0x00040000,
-        CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
-        CREATE_SEPARATE_WOW_VDM = 0x00001000,
-        CREATE_SHARED_WOW_VDM = 0x00001000,
-        CREATE_SUSPENDED = 0x00000004,
-        CREATE_UNICODE_ENVIRONMENT = 0x00000400,
-        DEBUG_ONLY_THIS_PROCESS = 0x00000002,
-        DEBUG_PROCESS = 0x00000001,
-        DETACHED_PROCESS = 0x00000008,
-        EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
-        INHERIT_PARENT_AFFINITY = 0x00010000
-    }
-
-    class MulticlientPatch : GWCAMemory
-    {
-
-        static public IntPtr mcmutexaddr = IntPtr.Zero;
-        static public IntPtr datfix1 = IntPtr.Zero;
-        static public IntPtr datfix2 = IntPtr.Zero;
-        static public IntPtr datfix2retn = IntPtr.Zero;
-
-        public MulticlientPatch(Process proc)
-            :base(proc)
-        {
-            InitScanner(new IntPtr(0x401000), 0x49A000);
-        }
-
-        ~MulticlientPatch()
-        {
-           TerminateScanner();
-        }
-
-        public bool ApplyMulticlientPatch()
-        {
-            return HandleManager.ClearMutex();
-        }
-
-        public bool ApplyMulticlientPatch_old()
-        {
-            if (mcmutexaddr == IntPtr.Zero)
-            {
-                mcmutexaddr = ScanForPtr(new byte[] { 0x8B, 0xF0, 0x85, 0xF6, 0x74, 0x10, 0xFF }, 0x14);
-                if (mcmutexaddr == IntPtr.Zero) return false;
-            }
-            WriteBytes(mcmutexaddr, new byte[] { 0xEB });
-
-            return true;
-        }
-
-        public bool ApplyDatFix()
-        {
-            if (datfix1 == IntPtr.Zero)
-            {
-                // signature of .dat "OpenFile" call.
-                datfix1 = ScanForPtr(new byte[] { 0x6A, 0x00, 0xBA, 0x00, 0x00, 0x00, 0xC0 }, 0x1);
-                if (datfix1 == IntPtr.Zero) return false;
-            }
-
-            // Change handle to allow others to open I/O with the same .dat
-            WriteBytes(datfix1, new byte[] { 0x03 });
-
-
-            if (datfix2 == IntPtr.Zero)
-            {
-                // WriteFile call.
-                datfix2 = ScanForPtr(new byte[] { 0x6A, 0x00, 0x52, 0x57, 0x50, 0x51 }, 0);
-                if (datfix2 == IntPtr.Zero) return false;
-                datfix2retn = (IntPtr)(datfix2.ToInt32() + 0x6);
-            }
-
-            // NOP it to avoid multiple writes, causing dat corruption
-              WriteBytes(datfix2, new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 }); 
-
-            return true;
-        }
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool CreateProcess(string lpApplicationName,
-              string lpCommandLine, IntPtr lpProcessAttributes,
-              IntPtr lpThreadAttributes,
-              bool bInheritHandles, ProcessCreationFlags dwCreationFlags,
-              IntPtr lpEnvironment, string lpCurrentDirectory,
-              ref STARTUPINFO lpStartupInfo,
-              out PROCESS_INFORMATION lpProcessInformation);
-
-        [DllImport("kernel32.dll")]
-        public static extern uint ResumeThread(IntPtr hThread);
+        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+        static extern uint ResumeThread(IntPtr hThread);
 
         static public Process LaunchClient(string path,string args,bool datfix)
         {
@@ -163,46 +50,26 @@ namespace GWMC_CS
                 return null;
             }
 
-            STARTUPINFO startup = new STARTUPINFO();
-            PROCESS_INFORMATION procinfo;
-
-            bool createprocessresult = CreateProcess(
-                path,
-                "\"" + path + "\"" + args,
-                IntPtr.Zero,
-                IntPtr.Zero,
-                false,
-                ProcessCreationFlags.CREATE_SUSPENDED,
-                IntPtr.Zero,
-                null,
-                ref startup,
-                out procinfo);
-
-            if (createprocessresult == false)
-            {
-                MessageBox.Show("Unable to launch Gw.exe.", "GWMC - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return null;
-            };
-            
+            IntPtr hThread = IntPtr.Zero;
+            uint dwPID = LaunchClient(path, null, null, null, args, (GWML_FLAGS)(datfix ? 2 : 3), out hThread);
 
 
-            Process proc = Process.GetProcessById((int)procinfo.dwProcessId);
-            MulticlientPatch patch = new MulticlientPatch(proc);
 
 
-            patch.ApplyMulticlientPatch_old();
-            if (datfix) patch.ApplyDatFix();
+
+            Process proc = Process.GetProcessById((int)dwPID);
+            GWCAMemory mem = new GWCAMemory(proc);
 
             string dllpath = Path.GetDirectoryName(path) + "\\plugins";
             if (Directory.Exists(dllpath)) {
                 string[] files = Directory.GetFiles(dllpath, "*.dll");
                 foreach(string file in files)
                 {
-                    patch.LoadModule(file);
+                    mem.LoadModule(file);
                 }
             }
 
-            ResumeThread(procinfo.hThread);
+            ResumeThread(hThread);
 
             return proc;
         }
