a8a9c12cf4f09208091771290e4df344fa00d6c9
unknown <github@skyweb.nu>
2022-06-01 22:15:49 +0200

    Remove useless spaces

--- GW Launcher/Forms/AddAccountForm.Designer.cs
+++ GW Launcher/Forms/AddAccountForm.Designer.cs
@@ -48,9 +48,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxTitle = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
+            /
             // labelEmail
-            // 
+            /
             this.labelEmail.AutoSize = true;
             this.labelEmail.Location = new System.Drawing.Point(23, 45);
             this.labelEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -58,26 +58,26 @@
             this.labelEmail.Size = new System.Drawing.Size(39, 15);
             this.labelEmail.TabIndex = 0;
             this.labelEmail.Text = "Email:";
-            // 
+            /
             // textBoxEmail
-            // 
+            /
             this.textBoxEmail.Location = new System.Drawing.Point(70, 42);
             this.textBoxEmail.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.textBoxEmail.Name = "textBoxEmail";
             this.textBoxEmail.Size = new System.Drawing.Size(168, 23);
             this.textBoxEmail.TabIndex = 1;
-            // 
+            /
             // textBoxPassword
-            // 
+            /
             this.textBoxPassword.Location = new System.Drawing.Point(70, 71);
             this.textBoxPassword.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
             this.textBoxPassword.Size = new System.Drawing.Size(135, 23);
             this.textBoxPassword.TabIndex = 2;
-            // 
+            /
             // labelPassword
-            // 
+            /
             this.labelPassword.AutoSize = true;
             this.labelPassword.Location = new System.Drawing.Point(29, 75);
             this.labelPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -85,17 +85,17 @@
             this.labelPassword.Size = new System.Drawing.Size(33, 15);
             this.labelPassword.TabIndex = 3;
             this.labelPassword.Text = "Pass:";
-            // 
+            /
             // textBoxCharacter
-            // 
+            /
             this.textBoxCharacter.Location = new System.Drawing.Point(70, 100);
             this.textBoxCharacter.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.textBoxCharacter.Name = "textBoxCharacter";
             this.textBoxCharacter.Size = new System.Drawing.Size(168, 23);
             this.textBoxCharacter.TabIndex = 4;
-            // 
+            /
             // labelCharacter
-            // 
+            /
             this.labelCharacter.AutoSize = true;
             this.labelCharacter.Location = new System.Drawing.Point(27, 103);
             this.labelCharacter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -103,9 +103,9 @@
             this.labelCharacter.Size = new System.Drawing.Size(35, 15);
             this.labelCharacter.TabIndex = 5;
             this.labelCharacter.Text = "Char:";
-            // 
+            /
             // checkBoxDatFix
-            // 
+            /
             this.checkBoxDatFix.AutoSize = true;
             this.checkBoxDatFix.Location = new System.Drawing.Point(13, 194);
             this.checkBoxDatFix.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -115,9 +115,9 @@
             this.checkBoxDatFix.Text = "Apply .dat Patch";
             this.checkBoxDatFix.UseVisualStyleBackColor = true;
             this.checkBoxDatFix.CheckedChanged += new System.EventHandler(this.CheckBoxDatFix_CheckedChanged);
-            // 
+            /
             // buttonDone
-            // 
+            /
             this.buttonDone.Location = new System.Drawing.Point(163, 220);
             this.buttonDone.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.buttonDone.Name = "buttonDone";
@@ -127,9 +127,9 @@
             this.buttonDone.UseCompatibleTextRendering = true;
             this.buttonDone.UseVisualStyleBackColor = true;
             this.buttonDone.Click += new System.EventHandler(this.ButtonDone_Click);
-            // 
+            /
             // labelPath
-            // 
+            /
             this.labelPath.AutoSize = true;
             this.labelPath.Location = new System.Drawing.Point(28, 137);
             this.labelPath.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -137,17 +137,17 @@
             this.labelPath.Size = new System.Drawing.Size(34, 15);
             this.labelPath.TabIndex = 8;
             this.labelPath.Text = "Path:";
-            // 
+            /
             // textBoxPath
-            // 
+            /
             this.textBoxPath.Location = new System.Drawing.Point(70, 133);
             this.textBoxPath.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.textBoxPath.Name = "textBoxPath";
             this.textBoxPath.Size = new System.Drawing.Size(135, 23);
             this.textBoxPath.TabIndex = 9;
-            // 
+            /
             // buttonDialogPath
-            // 
+            /
             this.buttonDialogPath.Location = new System.Drawing.Point(213, 133);
             this.buttonDialogPath.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.buttonDialogPath.Name = "buttonDialogPath";
@@ -156,9 +156,9 @@
             this.buttonDialogPath.Text = "...";
             this.buttonDialogPath.UseVisualStyleBackColor = true;
             this.buttonDialogPath.Click += new System.EventHandler(this.ButtonDialogPath_Click);
-            // 
+            /
             // labelExtraArguments
-            // 
+            /
             this.labelExtraArguments.AutoSize = true;
             this.labelExtraArguments.Location = new System.Drawing.Point(13, 165);
             this.labelExtraArguments.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -166,17 +166,17 @@
             this.labelExtraArguments.Size = new System.Drawing.Size(49, 15);
             this.labelExtraArguments.TabIndex = 12;
             this.labelExtraArguments.Text = "Ex Args:";
-            // 
+            /
             // textBoxExtraArguments
-            // 
+            /
             this.textBoxExtraArguments.Location = new System.Drawing.Point(70, 162);
             this.textBoxExtraArguments.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.textBoxExtraArguments.Name = "textBoxExtraArguments";
             this.textBoxExtraArguments.Size = new System.Drawing.Size(168, 23);
             this.textBoxExtraArguments.TabIndex = 13;
-            // 
+            /
             // buttonTogglePasswordVisibility
-            // 
+            /
             this.buttonTogglePasswordVisibility.Location = new System.Drawing.Point(213, 71);
             this.buttonTogglePasswordVisibility.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.buttonTogglePasswordVisibility.Name = "buttonTogglePasswordVisibility";
@@ -185,9 +185,9 @@
             this.buttonTogglePasswordVisibility.Text = "*";
             this.buttonTogglePasswordVisibility.UseVisualStyleBackColor = true;
             this.buttonTogglePasswordVisibility.Click += new System.EventHandler(this.ButtonTogglePasswordVisibility_Click);
-            // 
+            /
             // checkBoxElevated
-            // 
+            /
             this.checkBoxElevated.AutoSize = true;
             this.checkBoxElevated.Location = new System.Drawing.Point(13, 223);
             this.checkBoxElevated.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -196,9 +196,9 @@
             this.checkBoxElevated.TabIndex = 15;
             this.checkBoxElevated.Text = "Run elevated";
             this.checkBoxElevated.UseVisualStyleBackColor = true;
-            // 
+            /
             // buttonMods
-            // 
+            /
             this.buttonMods.Location = new System.Drawing.Point(163, 191);
             this.buttonMods.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.buttonMods.Name = "buttonMods";
@@ -207,9 +207,9 @@
             this.buttonMods.Text = "Mods";
             this.buttonMods.UseVisualStyleBackColor = true;
             this.buttonMods.Click += new System.EventHandler(this.ButtonMods_Click);
-            // 
+            /
             // label1
-            // 
+            /
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(30, 16);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
@@ -217,17 +217,17 @@
             this.label1.Size = new System.Drawing.Size(32, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Title:";
-            // 
+            /
             // textBoxTitle
-            // 
+            /
             this.textBoxTitle.Location = new System.Drawing.Point(70, 13);
             this.textBoxTitle.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.textBoxTitle.Name = "textBoxTitle";
             this.textBoxTitle.Size = new System.Drawing.Size(168, 23);
             this.textBoxTitle.TabIndex = 1;
-            // 
+            /
             // AddAccountForm
-            // 
+            /
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
