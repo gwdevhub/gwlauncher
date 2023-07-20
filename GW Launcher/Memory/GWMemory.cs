99581bf119ebbf61b75e7930b7dc2ef9c6adb9e3
DubbleClick <m@pyc.ac>
2023-07-20 22:15:59 +0700

    fix email ptr and charname ptr

--- GW Launcher/Memory/GWMemory.cs
+++ GW Launcher/Memory/GWMemory.cs
@@ -8,21 +8,18 @@ internal class GWMemory
 
     internal static void FindAddressesIfNeeded(GWCAMemory cli)
     {
-        IntPtr tmp;
         var imagebase = cli.GetImageBase();
         cli.InitScanner(imagebase.Item1, imagebase.Item2);
-        tmp = cli.ScanForPtr(new byte[] {0x55, 0x8B, 0xEC, 0x83, 0xEC, 0x10, 0x56, 0x6A}, 0x22, true);
-        if (tmp != IntPtr.Zero)
-        {
-            WinTitle = tmp;
-        }
 
-        tmp = cli.ScanForPtr(new byte[] {0x83, 0xC4, 0x40, 0x5F, 0x5E, 0x5B, 0x8B, 0x4D});
-        if (tmp != IntPtr.Zero)
-        {
-            EmailAddPtr = cli.Read<IntPtr>(new IntPtr(tmp.ToInt32() - 0x48));
-            CharnamePtr = cli.Read<IntPtr>(new IntPtr(tmp.ToInt32() - 0x2E));
-        }
+        WinTitle = cli.ScanForPtr(new byte[] { 0x55, 0x8B, 0xEC, 0x83, 0xEC, 0x10, 0x56, 0x6A }, 0x22, true);
+
+        EmailAddPtr =
+            cli.ScanForPtr(new byte[] { 0x33, 0xC0, 0x5D, 0xC2, 0x10, 0x00, 0xCC, 0x68, 0x80, 0x00, 0x00, 0x00 }, 0xE,
+                true);
+
+        CharnamePtr =
+            cli.ScanForPtr(new byte[] { 0x8B, 0xF8, 0x6A, 0x03, 0x68, 0x0F, 0x00, 0x00, 0xC0, 0x8B, 0xCF, 0xE8 }, -0x42,
+                true);
 
         cli.TerminateScanner();
     }
