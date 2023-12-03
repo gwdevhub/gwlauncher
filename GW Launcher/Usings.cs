f49413d0d9785ad840b4b00fa9066e0f31d79d03
DubbleClick <m@pyc.ac>
2023-12-03 23:24:12 +0100

    remove zip

--- GW Launcher/Usings.cs
+++ GW Launcher/Usings.cs
@@ -9,5 +9,4 @@ global using System.Reflection;
 global using System.Runtime.InteropServices;
 global using System.Text;
 global using System.Text.RegularExpressions;
-global using Ionic.Zip;
 global using Newtonsoft.Json;
