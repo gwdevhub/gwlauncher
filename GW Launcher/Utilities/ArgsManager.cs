132add51cb1ef9843759adfaf581d7007568cbc9
box-cs <foxablessc2@gmail.com>
2024-02-03 14:36:37 -0700

    added way to manage args

---
+++ GW Launcher/Utilities/ArgsManager.cs
@@ -0,0 +1,33 @@
+﻿namespace GW_Launcher.Utilities;
+
+internal static class ArgsManager {
+    private static Dictionary<string, string> _commandLineArgs;
+    static ArgsManager() {
+        _commandLineArgs = GetCommandLineArgs();
+    }
+    public static List<int> processProfileArgs() {
+        List<int> profilesToLaunch = new();
+        if (_commandLineArgs.ContainsKey("profiles")) { 
+            var profiles = _commandLineArgs["profiles"];
+            foreach (var profile in  profiles.Split(',')) {
+                if (int.TryParse(profile, out int profileValue)) {
+                    profilesToLaunch.Add(profileValue);
+                }
+            }
+        }
+        return profilesToLaunch;
+    }
+
+    private static Dictionary<string, string> GetCommandLineArgs() {
+        var argsDictionary = new Dictionary<string, string>();
+        var args = Environment.GetCommandLineArgs().Skip(1);
+        foreach (var chunk in args.Chunk(2)) {
+            if (chunk.Length == 2) {
+                argsDictionary[chunk.First()] = chunk.Last();
+            }
+        }
+        return argsDictionary;
+    }
+
+
+}
