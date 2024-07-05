3783fadf25358575eb4cf1e5249a9b97b972d1ed
DubbleClick <m@pyc.ac>
2024-07-05 12:54:35 +0200

    oopsie

--- GW Launcher/Guildwars/GwDownloader.cs
+++ GW Launcher/Guildwars/GwDownloader.cs
@@ -69,7 +69,7 @@ public static class GwDownloader
 
             var uniquePaths = accountsToUpdate.Select(a => a.gwpath).Distinct().ToList();
             progress.Report(("Copying Gw.exe to client paths", 0.9));
-            await CopyGwExeToAccountPaths(uniquePaths, new Progress<double>(p => progress.Report(("Copying Gw.exe to client paths", 0.9 + p * (1 / accountsToUpdate.Count())))), cancellationToken);
+            await CopyGwExeToAccountPaths(uniquePaths, new Progress<double>(p => progress.Report(("Copying Gw.exe to client paths", 0.9 + p * 0.1))), cancellationToken);
 
             progress.Report(("Update completed", 1));
         }
